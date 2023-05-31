1189c61b0830b1e3f84f2db54d9acc30f840bc5e
Matt MacFarlane <tidydata@outlook.com>
2023-05-31 11:46:34 -0400

    mock code challenge attempt!

--- server/app.py
+++ server/app.py
@@ -7,6 +7,7 @@ DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'instanc
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from datetime import datetime
 
 from models import db, Activity, Camper
 
@@ -23,5 +24,62 @@ db.init_app(app)
 def home():
     return ''
 
+@app.route('/campers', methods=['GET', 'POST'])
+def get_campers():
+    if request.method == 'GET':
+        campers = Camper.query.all() 
+        return {'campers': [camper.to_dict() for camper in campers]}
+    else:
+        campers = request.form['campers']
+
+
+@app.route('/campers/<int:id>', methods=['GET', 'DELETE'])
+def get_camper_by_id(id):
+    camper = Camper.query.get(1)
+    return camper.to_dict() 
+
+
+@app.route('/activities', methods=['GET'])
+def get_activities():
+    activities = Activity.query.all() 
+    return {'activities': [activity.to_dict() for activity in activities]}
+
+
+@app.route('/signups', methods=['POST'])
+def new_signup():
+    request_data = request.get_json()
+    time = request.form.get('time')
+    camper_id = request.form.get('camper_id')
+    activity_id = request.form.get('activity_id')
+    if request_data:
+        if 'time' in request_data:
+            time = request_data['time'] 
+        if 'camper_id' in request_data:
+            camper_id = request_data['camper_id']
+        if 'activity_id' in request_data: 
+            activity_id = request_data['activity_id']
+    return '''
+           The time value is: {}
+           The camper_id value is: {}
+           The activity_id value is: {}
+            '''.format(time, camper_id, activity_id)
+
+
+    
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+"""
+@app.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+    # handle the POST request
+    if request.method == 'POST':
+        time = request.form.get('time')
+        camper_id = request.form.get('camper_id')
+        activity
+        return '''
+                  <h1>The language value is: {}</h1>
+                  <h1>The framework value is: {}</h1>'''.format(language, framework)
+
+"""
--- server/models.py
+++ server/models.py
@@ -3,6 +3,9 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime 
+from sqlalchemy.sql import func
+
 
 convention = {
   "ix": "ix_%(column_0_label)s",
@@ -22,6 +25,11 @@ class Activity(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now()) 
+
+    campers = db.relationship('Camper', backref='activity')
 
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
@@ -32,6 +40,20 @@ class Camper(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now()) 
+
+    activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'))
+
+    @validates('camper')
+    def validate_camper(self, age, name):
+        if name and age >= 8 and age <= 18:
+            return name 
+        else:
+            raise ValueError('age must be between 8 - 18 and name must exist')
+        
+        
 
     def __repr__(self):
         return f'<Camper {self.id}: {self.name}>'
@@ -40,7 +62,24 @@ class Signup(db.Model, SerializerMixin):
     __tablename__ = 'signups'
 
     id = db.Column(db.Integer, primary_key=True)
+    camper_id = db.Column(db.Integer, unique=True)
+    activity_id = db.Column(db.Integer) 
+    time = db.Column(db.Integer) 
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
 
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now()) 
+
+    activities = db.relationship('Activity', foreign_keys=[Activity.id],
+                                 backref=db.backref('activity', lazy='joined'), lazy='dynamic')
+    
+    campers = db.relationship('Camper', foreign_keys=[Camper.id], 
+                              backref=db.backref('followed', lazy='joined'), lazy='dynamic')
+    
+    @validates('signup')
+    def validate_signup(self, time, name):
+        if time > 0 and time <= 23:
+            return name 
+        
     def __repr__(self):
         return f'<Signup {self.id}>'
 
